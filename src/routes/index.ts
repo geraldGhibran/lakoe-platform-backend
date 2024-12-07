43fe313897bff9dded5c7c97d7dac79d5daf917b
Gerald Ghibran <gerald.gunasha@gmail.com>
2024-12-07 13:31:46 +0700

    Fix:fixing-lakoe-route-store

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import variantRoute from './variant.route';
 import authRoute from './auth.route';
 import adminRoute from './admin.route';
 import locationRoute from './location.route';
+import storeRoute from './store.route';
 const router = Router();
 
 router.use('/auth', authRoute);
@@ -12,5 +13,6 @@ router.use('/varaint', variantRoute);
 router.use('/auth', authRoute);
 router.use('/admin', adminRoute);
 router.use('/location', locationRoute);
+router.use('/store', storeRoute);
 
 export default router;
