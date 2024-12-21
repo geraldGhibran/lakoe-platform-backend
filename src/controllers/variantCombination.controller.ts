93661b53e80915866b52a0439f11feee7a434566
Kafi Abdur Rahim <kafiabdurrahim@gmail.com>
2024-12-21 20:15:11 +0700

    feat:update variant combination

--- src/controllers/variantCombination.controller.ts
+++ src/controllers/variantCombination.controller.ts
@@ -1,1 +1,21 @@
 import { Request, Response } from 'express';
+
+import * as variantCombinationService from '../services/variantItemValue.service';
+
+export const updateVariantItemValue = async (req: Request, res: Response) => {
+  try {
+    const { variantCombinationIds, ...rest } = req.body;
+
+    console.log(variantCombinationIds);
+    const result = variantCombinationIds.forEach(async (variantId: number) => {
+      await variantCombinationService.updateVariantItemValue(rest, variantId);
+    });
+    res.send({
+      message: 'update success',
+      result: result,
+    });
+  } catch (error) {
+    const err = error as Error;
+    res.status(500).send(err.message);
+  }
+};
