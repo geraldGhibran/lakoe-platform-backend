a3fed49361f25b9fbafbcbc43228ea01eb43afcd
HP <faisalyulianto26@gmail.com>
2024-12-27 22:40:19 +0700

    fix

--- prisma/migrations/20241223131834_fix/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Courier" ADD COLUMN     "storeId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Courier" ADD CONSTRAINT "Courier_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store"("id") ON DELETE SET NULL ON UPDATE CASCADE;
