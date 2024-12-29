7ed7948a8efaa4b305339b7ad4cf14f231836171
Gerald Ghibran <gerald.gunasha@gmail.com>
2024-12-29 12:09:12 +0700

    chore:deploy-to-be-8

--- dist/libs/validator/registerSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerSchema = void 0;
-const zod_1 = require("zod");
-exports.registerSchema = zod_1.z.object({
-    name: zod_1.z.string().min(3, 'Name at least 3 character'),
-    email: zod_1.z.string().email('Invalid email address'),
-    password: zod_1.z.string().min(6, 'Password at least 6 character'),
-});
